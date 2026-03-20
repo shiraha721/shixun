70595ae7cd0053c4217e3d27d449a3a01291793a
Sakris <llx33550336@gmail.com>
2026-03-20 17:19:45 +0800

    Add files via upload

---
+++ campus-product-sys/src/main/java/com/shixun/service/DashboardService.java
@@ -0,0 +1,17 @@
+package com.shixun.service;
+
+import com.shixun.pojo.api.Result;
+import com.shixun.pojo.vo.ChartVO;
+
+import java.util.List;
+
+/**
+ * 仪表盘业务逻辑接口
+ */
+public interface DashboardService {
+
+    Result<List<ChartVO>> staticCount();
+
+    Result<List<ChartVO>> productShelvesInfo(Integer day);
+
+}
